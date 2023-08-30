2980a7ecef8eedc13be3076bf1cfe4ec3c98b101
SambitAmanta <69429176+SambitAmanta@users.noreply.github.com>
2023-08-30 18:44:06 +0530

    Update Navbar.tsx

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -62,7 +62,7 @@ function Navbar() {
             ease: "easeInOut",
           }}
         >
-          <Link href="kishansheth.com">
+          <Link href="sambitamanta.me">
             <Logo />
           </Link>
         </motion.div>
